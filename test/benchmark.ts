9d0504fa059fd81bceda09015dc1f363f0b317af
Jim de Beer <beerdejim@gmail.com>
2022-09-28 11:19:26 +0200

    more bench marks

--- test/benchmark.ts
+++ test/benchmark.ts
@@ -4,9 +4,43 @@ import deepdiff, { applyChange } from 'deep-diff'
 import { deepCopy, deepEqual } from '@saulx/utils'
 import { updatedDiff } from 'deep-object-diff'
 
+test('Simple benchmark', async (t) => {
+  const start = {
+    a: 'hello',
+    b: 'shurf',
+    c: 'snurx',
+    d: { e: 'x' },
+    f: [1, 2, 3, 4, 5],
+  }
+  const dest = {
+    a: 'BLARF',
+    z: true,
+    f: [6, 1, 2, 8, 9, 4, 5],
+    snurkypants: { a: true, b: false },
+    d: { e: { x: true } },
+  }
+
+  const patch = diff(start, dest)
+
+  const copies: any = []
+  for (let i = 0; i < 1000e3; i++) {
+    copies.push(deepCopy(start))
+  }
+
+  const d = Date.now()
+
+  for (let i = 0; i < 1000e3; i++) {
+    applyPatch(copies[i], patch)
+  }
+
+  console.info(Date.now() - d, 'ms')
+
+  t.pass()
+})
+
 test('Benchmark', async (t) => {
-  const a = { a: true, b: false, z: [1, 3, 5], x: [] }
-  const b = { a: true, b: { x: true }, z: [1, 2, 3, 4, 5], x: [] }
+  const a: any = { a: true, b: false, z: [1, 3, 5], x: [] }
+  const b: any = { a: true, b: { x: true }, z: [1, 2, 3, 4, 5], x: [] }
 
   for (let i = 0; i < 100; i++) {
     if (i % 2) {
@@ -15,9 +49,9 @@ test('Benchmark', async (t) => {
     b.x.push({ bla: true, i, flapper: false, snurkypants: true })
   }
 
-  const copiesD = []
-  const copiesS = []
-  const copiesX = []
+  const copiesD: any = []
+  const copiesS: any = []
+  const copiesX: any = []
 
   for (let i = 0; i < 10000; i++) {
     copiesS.push(deepCopy(a))
@@ -25,30 +59,30 @@ test('Benchmark', async (t) => {
     copiesX.push(deepCopy(a))
   }
 
-  const xx = []
-  const xxx = []
-  const xxxx = []
+  const xx: any = []
+  const xxx: any = []
+  const xxxx: any = []
 
   let d = Date.now()
   for (let i = 0; i < 10000; i++) {
     const y = updatedDiff(copiesX[i], b)
     xxxx.push(y)
   }
-  console.log('Deep-diff-object create patch', Date.now() - d, 'ms')
+  console.info('Deep-diff-object create patch', Date.now() - d, 'ms')
 
   d = Date.now()
   for (let i = 0; i < 10000; i++) {
     const y = deepdiff(copiesD[i], b)
     xxx.push(y)
   }
-  console.log('Deep-diff create patch', Date.now() - d, 'ms')
+  console.info('Deep-diff create patch', Date.now() - d, 'ms')
 
   d = Date.now()
   for (let i = 0; i < 10000; i++) {
     const y = diff(copiesS[i], b)
     xx.push(y)
   }
-  console.log('Saulx-diff create patch', Date.now() - d, 'ms')
+  console.info('Saulx-diff create patch', Date.now() - d, 'ms')
 
   // console.dir(xx[xx.length - 1], { depth: 10 })
 
@@ -58,19 +92,19 @@ test('Benchmark', async (t) => {
     JSON.stringify(xx[xx.length - 1]).length /
     JSON.stringify(xxx[xxx.length - 1]).length
 
-  //   console.log(JSON.stringify(xxx[xxx.length - 1]))
+  //   console.info(JSON.stringify(xxx[xxx.length - 1]))
 
-  console.log(`Saulx diff is ${~~(ratio * 100)}% smaller then deep-diff`)
+  console.info(`Saulx diff is ${~~(ratio * 100)}% smaller then deep-diff`)
 
   d = Date.now()
   for (let i = 0; i < 10000; i++) {
     const y = applyPatch(copiesS[i], xx[i])
 
     if (i === 10000 - 1) {
-      console.log('Saulx diff creates equal', deepEqual(y, b))
+      console.info('Saulx diff creates equal', deepEqual(y, b))
     }
   }
-  console.log('Saulx diff application', Date.now() - d, 'ms')
+  console.info('Saulx diff application', Date.now() - d, 'ms')
 
   d = Date.now()
   for (let i = 0; i < 10000; i++) {
@@ -78,11 +112,11 @@ test('Benchmark', async (t) => {
       applyChange(copiesD[i], xxx[i][j])
     }
     if (i === 10000 - 1) {
-      console.log('Deep diff creates equal', deepEqual(copiesD[i], b))
+      console.info('Deep diff creates equal', deepEqual(copiesD[i], b))
     }
   }
 
-  console.log('Deep diff application', Date.now() - d, 'ms')
+  console.info('Deep diff application', Date.now() - d, 'ms')
 
   t.pass()
 })
